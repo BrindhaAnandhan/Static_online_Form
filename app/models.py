4cd0302591626bbec40b9699de6e909c25c89ec1
BrindhaAnandhan <brindhaorbindu@gmail.com>
2024-01-05 12:11:04 +0530

    Personal details completed

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Information(models.Model):
+    First_Name = models.CharField(max_length = 100, primary_key = True)
+    Middle_Name = models.CharField(max_length = 100, blank = True)
+    Father_Name = models.CharField(max_length = 100)
+    Mother_Name = models.CharField(max_length = 100)
+    Current_Address = models.TextField()
+    Permanent = models.TextField()
+    Phone_number = models.IntegerField(unique = True)
+    Land_line = models.IntegerField(blank = True)
+    Date_of_Birth = models.DateField(auto_now_add=True)
+    Place_of_Birth = models.DateField(blank = True)
+    
+    def __str__(self):
+        return self.First_Name
+
--- app/views.py
+++ app/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from app.models import *
 
 # Create your views here.
 def online_form(request):
+    if request.method == 'POST':
+        First = request.POST['First']
+        Middle = request.POST['Middle']
+        Father = request.POST['Father']
+        Mother = request.POST["Mother"]
+        Current = request.POST['Current']
+        Permanent = request.POST["Permanent"]
+        Phone = request.POST["Phone"]
+        Landline = request.POST['Landline']
+        DOB = request.POST['DOB']
+        POB = request.POST["POB"]
+        OFO = Information.objects.get_or_create(First_Name= First, Middle_Name = Middle, Father_Name= Father, Mother_Name = Mother, Current_Address = Current, Permanent = Permanent, Phone_number = Phone, Land_line = Landline, Date_of_Birth= DOB,  Place_of_Birth = POB )[0]
+        OFO.save()
+        OFo = Information.objects.all()
+        data = {'onlineForm' : OFo}
+        return render(request, 'display.html', data)
+
     return render(request,'online_form.html')
